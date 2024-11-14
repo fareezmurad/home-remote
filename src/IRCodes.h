a40e5d9117ff8d8379cc9dd4959c87694fdd4d8c
fareezmurad <fareezphobia@gmail.com>
2024-11-14 22:54:15 +0800

    Add Daikin AC functions

--- src/IRCodes.h
+++ src/IRCodes.h
@@ -1,15 +1,14 @@
 #ifndef IRCODES_H
 #define IRCODES_H
 
-#include <stdint.h>
 #include <ESP32Encoder.h>
 #include <U8g2lib.h>
+#include <stdint.h>
 
 extern U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2;
 extern ESP32Encoder rotaryEncoder;
 extern int encoderCurrentRead;
 extern int encoderLastRead;
-
 
 // Structure for storing IR command details
 struct SymphonyCode {
@@ -24,11 +23,21 @@ void initIrSend();
 // Sends IR command to control Deka fan speed
 void dekaSpeedControl(int index);
 
-// Command to control the Sharp AC
+// Command to control Sharp air-conditioner
 void sharpAcSetOn();
 void sharpAcSetOff();
 void sharpAcSetTempUI();
 void sharpAcSetFanUI();
 void sharpAcSetSwingUI();
+void sharpAcChkInactivity();
 
-#endif  // IRCODES_H
+// Command to control Daikin air-conditioner
+void daikinAcUI();
+void daikinAcToggleOn();
+void daikinAcSetTempUI();
+void daikinAcSetModeUI();
+void daikinAcSetFanUI();
+void daikinAcSetSwingUI();
+void daikinAcChkInactivity();
+
+#endif  // IRCODES
