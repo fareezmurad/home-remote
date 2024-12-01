c5ed9421bb94dd08e8fd7de81436fda5e90c621d
fareezmurad <fareezphobia@gmail.com>
2024-12-01 14:35:54 +0800

    Add ESPNOW codes.

---
+++ src/ESPNOW.cpp
@@ -0,0 +1,68 @@
+#include "ESPNOW.h"
+
+#define SELECT_BUTTON 34
+
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x10, 0x06, 0x1C, 0xB5, 0x43, 0xE0};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  bool ledState;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void initESPNow() {
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Transmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void sendData() {
+  int buttonState = digitalRead(SELECT_BUTTON);
+  // Set values to send
+  myData.ledState = buttonState;
+
+  if (buttonState == LOW) {
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+    delay(500);
+  }
+}
---
+++ src/ESPNOW.h
@@ -0,0 +1,10 @@
+#ifndef ESPNOW_H
+#define ESPNOW_H
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+void initESPNow();
+void sendData();
+
+#endif
