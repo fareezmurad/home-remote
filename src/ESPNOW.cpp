3cea5a270ceea5f2ff4f4fa3d8b2b98c7c9474d3
beMiracle. <fareezphobia@gmail.com>
2024-12-25 16:10:31 +0800

    Format and wording correction.

--- src/ESPNOW.cpp
+++ src/ESPNOW.cpp
@@ -20,7 +20,7 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   memcpy(&switchData, incomingData, sizeof(switchData));
 }
 
-// callback when data is sent
+// Callback function when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
